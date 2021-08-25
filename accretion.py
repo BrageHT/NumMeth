0cf0c7b22b87c0b4e2cb691352fd1919b032be94
jonasoep <jonas.op@gmail.com>
2021-08-25 22:38:07 +0200

    Whole shit may work

--- accretion.py
+++ accretion.py
@@ -33,30 +33,12 @@ bb = y_diff(4, 1)
 cc = x_diff(4, 1)
 #%%
 @numba.njit
-def accretion_disc(N, x_start, x_end, t_start, t_end, dt, v):
+def accretion_disc(N, x_start, x_end, t_start, t_end, dt, v, ite):
     x = np.linspace(x_start, x_end, N)
     t = np.arange(t_start, t_end, dt)
     dx = x[1] - x[0]
     ux_init = np.zeros(N**2)
     uy_init = np.zeros(N**2)
-    
-    
-    
-    L = sp.lil_matrix(laplacian(N, dx))
-    L[0:N] = 0
-    L[-N:] = 0
-    for i in range(N):
-        L[i, i] = 1
-        L[-(i+1), -(i+1)] = 1
-    for i in range(N-2):
-        L[(i+1)*N] = 0
-        L[(i+1)*N, (i+1)*N] = 1
-        L[(i+2)*N-1] = 0
-        L[(i+2)*N-1, (i+2)*N-1] = 1
-    
-    A1 = sp.identity(N**2)-v*dt*L
-    visc_x = sp.solve(A1, ux_init)
-    visc_y = sp.solve(A1, uy_init)
     
     def advection_term(ux, uy):
         ux_mat = np.reshape(ux, (N, N))
@@ -94,14 +76,38 @@ def accretion_disc(N, x_start, x_end, t_start, t_end, dt, v):
                                 w22 * uy_mat[x2_index, y2_index])
         return np.flatten(ux_new), np.flatten(uy_new)
     
-    adv_x, adv_y = advection_term(visc_x, visc_y)
+    L = laplacian(N, dx)
     
-    del_x = sp.lil_matrix(x_diff(N, dx))
+    del_x = x_diff(N, dx)
+    del_y = y_diff(N, dx)
     
+    A1 = np.identity(N**2)-v*dt*L
+    A2 = L*dt
     
-    del_y = sp.lil_matrix(y_diff(N, dx))
+    inv_A1 = np.linalg.inv(A1)
+    inv_A2 = np.linalg.inv(A2)
     
-    p = del_x.dot(x_adv) + np.matmul(y_diff(N, dx), adv_y)
+    ux = np.zeros((ite, N, N))
+    uy = np.zeros((ite, N, N))
     
+    ux[0] = np.reshape(ux_init, (N, N))
+    uy[0] = np.reshape(uy_init, (N, N))
     
+    for i in tqdm(range(ite)):
+        
+        visc_x = inv_A1 @ ux_init
+        visc_y = inv_A1 @ uy_init
+        
+        adv_x, adv_y = advection_term(visc_x, visc_y)
+        
+        pres_term = del_x @ adv_x + del_x @ adv_y
+        
+        p = inv_A2 @ pres_term
+        
+        ux_init = adv_x - dt * del_x @ p
+        uy_init = adv_y - dt * del_y @ p
+        
+        ux[i+1] = np.reshape(ux_init, (N, N))
+        uy[i+1] = np.reshape(uy_init, (N, N))
     
+    return ux, uy
