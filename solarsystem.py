49aeecf5e7af97c7f1ecf94a8dcf206b8341533c
BrageHT <thomsen.brage@gmail.com>
2021-08-25 14:24:20 +0200

    Support for planet merge

--- solarsystem.py
+++ solarsystem.py
@@ -26,35 +26,72 @@ class Planet:
 
 #%%
 
-#defining diff eq
 
-def timestep2(planet,dt):
-    N = len(planet)
+def merge(planet,r,list_mask):
     
-    r = np.zeros((N,N)) # r from low to high planet no.
-    r_x = np.zeros((N,N)) # same
-    r_y = np.zeros((N,N)) # same
-    cos_theta = np.zeros((N,N))
-    sin_theta = np.zeros((N,N))
-    a = np.zeros((2,N))
+    close, _ = np.where(np.abs(r - 1/2) < 1/2) # planet a & b within 1 unit distance
+    if len(close) != 0: # and close.any() != list_except.any():
+        # print(close)
+        # conservation of momentum
+        m_final = planet[close[0]].mass + planet[close[1]].mass
+        v_final = (planet[close[0]].vel*planet[close[0]].mass + 
+                   planet[close[1]].vel*planet[close[1]].mass) / m_final
+        
+        planet[close[0]].mass = m_final
+        planet[close[0]].vel = v_final
+        
+        planet[close[1]].mass = 0
+        planet[close[1]].vel = 0
+        # print(*close)
+        list_mask[close[1]] = 0
+        print(f"Planets {close[0]} and  {close[1]} merged! ")
+        
+
+def timestep(planet,dt,first_run):
+    """
+    Explicit Euler for timestepping newtonian gravity
+    """
     
+    # Initializing and creating matricies before the first timestep
+    if first_run:
+        N = len(planet)
+        r = np.zeros((N,N)) # r from low to high planet no.
+        r_x = np.zeros((N,N)) # same
+        r_y = np.zeros((N,N)) # same
+        cos_theta = np.zeros((N,N))
+        sin_theta = np.zeros((N,N))
+        a = np.zeros((2,N))
+        first_run = False
+        
+        global list_mask
+        list_mask = np.ones(N)
+        
     for i in range(N):
-        for j in range(N):
-            if i != j:
-                
-                r_x[i,j] = planet[i].pos[0] - planet[j].pos[0]
-                r_y[i,j] = planet[i].pos[1] - planet[j].pos[1]
-                r[i,j] = np.sqrt(r_x[i,j]**2 + r_y[i,j]**2)
-                cos_theta[i,j] = r_x[i,j]/r[i,j]
-                sin_theta[i,j] = r_y[i,j]/r[i,j]
+        if list_mask[i]:
+            for j in range(N):
+                if list_mask[j]:
+                    if i != j: # and i < j:
+                        
+                        r_x[i,j] = planet[i].pos[0] - planet[j].pos[0]
+                        r_y[i,j] = planet[i].pos[1] - planet[j].pos[1]
+                        r[i,j] = np.sqrt(r_x[i,j]**2 + r_y[i,j]**2)
+                        cos_theta[i,j] = r_x[i,j]/r[i,j]
+                        sin_theta[i,j] = r_y[i,j]/r[i,j]
+        
+                        
+                        a[:,i] += -planet[j].mass/r[i,j]**2 * np.array( [ cos_theta[i,j], sin_theta[i,j] ] )
 
-                
-                a[:,i] += -planet[j].mass/r[i,j]**2 * np.array( [ cos_theta[i,j], sin_theta[i,j] ] )
-                # print(a[:,i])
-                
-        planet[i].vel = a[:,i]*dt
-        planet[i].pos += planet[i].vel
-        # print(planet[2].pos)
+    # print(first_run)
+    for i in range(N):
+        if list_mask[i]:
+            planet[i].vel = a[:,i]*dt
+            planet[i].pos += planet[i].vel
+    
+    # print(list_except)
+    merge(planet,r,list_mask)
+    
+    # print(planets[0].mass)
+    # print(planet[2].pos)
 
         
 
@@ -63,27 +100,28 @@ def timestep2(planet,dt):
 #%%
 plt.close("all")
 
-sun = Planet(1,(5.0,-10.0),(0.02,0.0))
-earth = Planet(1,(-5.0,-5.0),(0.0,-0.05)) #1/333000
-other = Planet(1,(10.0,5.0), (0.0,0.05))
-other2 = Planet(1, (-12.0,3.0),(0.06,0.0))
+sun = Planet(1,(2.0,2.0),(0.02,0.0))
+earth = Planet(1,(0.0,0.0),(0.0,-0.05)) #1/333000
+other = Planet(1,(5.0,5.0), (0.0,0.05))
+other2 = Planet(1, (-8.0,3.0),(0.06,0.0))
 
 planets = [earth,sun,other,other2]
+# planets = [earth,sun]
 fig = plt.figure(0)
 ax = fig.add_subplot(1,1,1)
-zoom = 20
-
-for i in range(1000):
-    
+zoom = 15
+first_run = True
+for i in range(300):
+    timestep(planets,0.1,first_run)
     ax.cla()
-    ax.plot(*earth.pos,'o',scalex=False,scaley=False)
-    ax.plot(*sun.pos,'o',scalex=False,scaley=False)
-    ax.plot(*other.pos,'o',scalex=False,scaley=False)
-    ax.plot(*other2.pos,'o',scalex=False,scaley=False)
+    for j in range(len(planets)):
+        if list_mask[j]:
+            ax.plot(*planets[j].pos,'o',scalex=False,scaley=False)
+
     ax.set(xlim=(-zoom,zoom), ylim=(-zoom,zoom))
     clear_output(wait = True)
     plt.pause(0.05)
-    timestep2(planets,0.5)
+    
 
 
 #%%
