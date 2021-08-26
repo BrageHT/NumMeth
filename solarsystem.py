94f9424de5ddca1efd4cbd0c78f1fd56b59f3974
BrageHT <thomsen.brage@gmail.com>
2021-08-26 15:00:17 +0200

    Added random planet support, improved merge for star

--- solarsystem.py
+++ solarsystem.py
@@ -4,7 +4,8 @@ Created on Fri Aug 20 13:06:13 2021
 
 @author: brage
 """
-
+from tqdm import tqdm
+from numba import jit
 import numpy as np
 import matplotlib.pyplot as plt
 # import scipy as sp
@@ -17,8 +18,8 @@ Defining classes
 class Planet:
     def __init__(self,mass,pos,vel):
         self.mass = mass
-        self.pos = np.array(pos)
-        self.vel = np.array(vel)
+        self.pos = pos
+        self.vel = vel
         
 #%%
 
@@ -26,15 +27,23 @@ class Planet:
 
 #%%
 
-
+# @jit
 def merge(planet,r,list_mask):
     """
     Calculating distance between planets and merging if close enough,
     and setting (highest) mask index of planet to zero.
     """
+    
     close, _ = np.where(np.abs(r - 1/2) < 1/2) # planet a & b within 1 unit distance
-    if len(close) != 0: # and close.any() != list_except.any():
+    close_star = np.where(np.abs(r[:,0] - 3/2) < 3/2) # larger merge radius for star (3 unit dist)
+    # print(close_star)
+    
+    
+    if len(close_star) > 0 and np.any(close_star) == np.any(np.where(list_mask == 1)):
+        close = np.array([0,*close_star[0]])
         # print(close)
+        
+    if len(close) != 0 and np.any(close) == np.any(np.where(list_mask == 1)):
         # conservation of momentum
         m_final = planet[close[0]].mass + planet[close[1]].mass
         v_final = (planet[close[0]].vel*planet[close[0]].mass + 
@@ -47,10 +56,11 @@ def merge(planet,r,list_mask):
         planet[close[1]].vel = 0
         # print(*close)
         list_mask[close[1]] = 0
+        list_mask[close[0]] = 1
         # print(f"Planets {close[0]} and  {close[1]} merged! ")
-        # print([planet[close[0]].mass, timestep(planets,0.1,first_run)])
         
-
+        
+# @jit
 def timestep(planet,dt,first_run):
     """
     Explicit Euler for timestepping newtonian gravity
@@ -89,7 +99,7 @@ def timestep(planet,dt,first_run):
 
     for i in range(N):
         if list_mask[i]:
-            planet[i].vel = a[:,i]*dt
+            planet[i].vel += a[:,i]*dt
             planet[i].pos += planet[i].vel
     
 
@@ -97,37 +107,58 @@ def timestep(planet,dt,first_run):
 
 
         
-
+#%%
+# @jit
+def random_init(N,mass_range,pos_range):
+    planets = []
+    
+    # Star in origo
+    star_mass = 500
+    planets.append(Planet(star_mass,(0.0,0.0),(0.0,0.0)))
+    
+    # creating N-1 planetesimals
+    for i in range(N-1):
+        
+        pos = np.random.uniform(*pos_range,2)
+        dist = np.sqrt(pos[0]**2 + pos[1]**2)
+        
+        #calculating v_int for a (somewhat) stable orbit
+        vel = np.sqrt(star_mass/dist)/10 * np.array([pos[1]/dist, pos[0]/dist] ) # sine and cos
+        # print(vel)
+        planets.append(Planet(np.random.uniform(*mass_range), pos, vel))
+        
+        
+    return planets
     
 
 #%%
 plt.close("all")
 
-sun = Planet(1,(2.0,2.0),(0.02,0.0))
-earth = Planet(1,(0.0,0.0),(0.0,-0.05)) #1/333000
-other = Planet(1,(5.0,5.0), (0.0,0.05))
-other2 = Planet(1, (-4.0,3.0),(0.06,0.0))
-
-planets = [earth,sun,other,other2]
-# planets = [earth,sun]
-
 fig = plt.figure(0)
 ax = fig.add_subplot(1,1,1)
-zoom = 15
+zoom = 30
 
-
+#Defining initial conditions
+planets_array = random_init(50,(0.1,5),(-zoom,zoom))
 first_run = True
-list_mask = np.ones(len(planets))
+list_mask = np.ones(len(planets_array))
 
 
-for i in range(300):
+for i in tqdm(range(1000)):
     
-    timestep(planets,0.1,first_run)
+    timestep(planets_array,0.01,first_run)
     ax.cla()
-    
-    for j in range(len(planets)):
+    # ax.legend()
+    list_mask[0] = 1
+    # print(planets_array[0].vel)
+    for j in range(len(planets_array)):
         if list_mask[j]:
-            ax.plot(*planets[j].pos,'o',scalex=False,scaley=False)
+            ax.plot(*planets_array[j].pos,marker='o', markersize=2*np.sqrt(planets_array[j].mass/np.pi),scalex=False,scaley=False)
+            # if j == 0:
+            #     ax.legend()
+            #     ax.plot(*planets[j].pos,'o',scalex=False,scaley=False,label='Star')
+            # else:
+            #     ax.plot(*planets[j].pos,'o',scalex=False,scaley=False)
 
     ax.set(xlim=(-zoom,zoom), ylim=(-zoom,zoom))
     clear_output(wait = True)
