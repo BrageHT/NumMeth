33aa2c7d0d26e3ea8b6ee30684d8b10dcfb830cb
jonasoep <jonas.op@gmail.com>
2021-08-26 15:30:51 +0200

    Wrong accretions with cool plots

--- accretion.py
+++ accretion.py
@@ -1,6 +1,6 @@
 import numpy as np
-import matplotlib.pyplot
-import tqdm
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 from scipy import sparse as sp
 import numba
 
@@ -29,16 +29,35 @@ def x_diff(N, dx):
 def y_diff(N, dx):
     A = np.eye(N**2, N**2, N) - np.eye(N**2, N**2, -N)
     return A/(2*dx)
-bb = y_diff(4, 1)
-cc = x_diff(4, 1)
+
 #%%
-@numba.njit
-def accretion_disc(N, x_start, x_end, t_start, t_end, dt, v, ite):
+
+hx, hy = np.meshgrid(np.linspace(-20,20,71), np.linspace(-20,20,71))
+
+u = -5*hy/np.sqrt(hy**2 + hx**2+0.00001)
+z = 5*hx /np.sqrt(hy**2 + hx**2+0.00001)
+
+
+#%%
+# @numba.njit
+def accretion_disc(N, x_start, x_end, dt, v, ite):
     x = np.linspace(x_start, x_end, N)
-    t = np.arange(t_start, t_end, dt)
     dx = x[1] - x[0]
+    
+    # ux_init = u.flatten()
+    # uy_init = z.flatten()
     ux_init = np.zeros(N**2)
     uy_init = np.zeros(N**2)
+    
+    r = np.zeros((N,N))
+    for i in range(N):
+        for j in range(N):
+            r[i, j] = np.sqrt((x[i])**2+(x[j])**2)
+    fx = np.array([[-0.001*x[i]/(r[i,j]**3+0.001) for i in range(N)] for j in range(N)])
+    fy = np.array([[-0.1*x[j]/(r[i,j]**3+0.001) for i in range(N)] for j in range(N)])
+    
+    fxx = fx.flatten()
+    fyy = fy.flatten()
     
     def advection_term(ux, uy):
         ux_mat = np.reshape(ux, (N, N))
@@ -51,30 +70,44 @@ def accretion_disc(N, x_start, x_end, t_start, t_end, dt, v, ite):
         
         for i in range(N):
             for j in range(N):
-                x_approx = (x_adv[i, j] - x_start)/dx
-                y_approx = (y_adv[i, j] - x_start)/dx
-                x1_index = int(x_approx)
-                x2_index = int(np.ceil(x_approx))
-                y1_index = int(y_approx)
-                y2_index = int(np.ceil(y_approx))
-                
-                w11 = abs((x[x2_index] - x_adv[i, j])*(x[y2_index] - y_adv[i, j]) / 
-                          ((x[x2_index] - x[x1_index])*(x[y2_index] - x[y1_index])))
-                w12 = abs((x[x2_index] - x_adv[i, j])*(x[y1_index] - y_adv[i, j]) / 
-                          ((x[x2_index] - x[x1_index])*(x[y2_index] - x[y1_index])))
-                w21 = abs((x[x1_index] - x_adv[i, j])*(x[y2_index] - y_adv[i, j]) / 
-                          ((x[x2_index] - x[x1_index])*(x[y2_index] - x[y1_index])))
-                w22 = 1 - w11 - w12 - w21
-                
-                ux_new[i, j] = (w11 * ux_mat[x1_index, y1_index] + 
-                                w12 * ux_mat[x1_index, y2_index] + 
-                                w21 * ux_mat[x2_index, y1_index] + 
-                                w22 * ux_mat[x2_index, y2_index])
-                uy_new[i, j] = (w11 * uy_mat[x1_index, y1_index] + 
-                                w12 * uy_mat[x1_index, y2_index] + 
-                                w21 * uy_mat[x2_index, y1_index] + 
-                                w22 * uy_mat[x2_index, y2_index])
-        return np.flatten(ux_new), np.flatten(uy_new)
+                if np.isnan(x_adv[i,j]):
+                    ux_new[i,j] = 0
+                elif np.isnan(y_adv[i,j]):
+                    uy_new[i,j] = 0
+                else:
+                    x_approx = (x_adv[i, j] - x_start)/dx
+                    y_approx = (y_adv[i, j] - x_start)/dx
+                    x1_index = int(x_approx)
+                    x2_index = x1_index +1
+                    y1_index = int(y_approx)
+                    y2_index = y1_index +1
+                    
+                    try:
+                        
+                        # print((x1_index, x2_index), (y1_index, y2_index))
+                        # print(x_adv[i,j],y_adv[i,j])
+                        
+                        w11 = abs((x[x2_index] - x_adv[i, j])*(x[y2_index] - y_adv[i, j]) / 
+                                  ((x[x2_index] - x[x1_index])*(x[y2_index] - x[y1_index])))
+                        w12 = abs((x[x2_index] - x_adv[i, j])*(x[y1_index] - y_adv[i, j]) / 
+                                  ((x[x2_index] - x[x1_index])*(x[y2_index] - x[y1_index])))
+                        w21 = abs((x[x1_index] - x_adv[i, j])*(x[y2_index] - y_adv[i, j]) / 
+                                  ((x[x2_index] - x[x1_index])*(x[y2_index] - x[y1_index])))
+                        w22 = 1 - w11 - w12 - w21
+                        
+                        ux_new[i, j] = (w11 * ux_mat[x1_index, y1_index] + 
+                                        w12 * ux_mat[x1_index, y2_index] + 
+                                        w21 * ux_mat[x2_index, y1_index] + 
+                                        w22 * ux_mat[x2_index, y2_index])
+                        uy_new[i, j] = (w11 * uy_mat[x1_index, y1_index] + 
+                                        w12 * uy_mat[x1_index, y2_index] + 
+                                        w21 * uy_mat[x2_index, y1_index] + 
+                                        w22 * uy_mat[x2_index, y2_index])
+                    except IndexError:
+                        ux_new[i, j] = 0
+                        uy_new[i, j] = 0
+                        
+        return ux_new.flatten(), uy_new.flatten()
     
     L = laplacian(N, dx)
     
@@ -82,27 +115,61 @@ def accretion_disc(N, x_start, x_end, t_start, t_end, dt, v, ite):
     del_y = y_diff(N, dx)
     
     A1 = np.identity(N**2)-v*dt*L
+    A1[0:N] = 0
+    A1[-N:] = 0
+    for i in range(N):
+        A1[i, i] = 1
+        A1[-(i+1), -(i+1)] = 1
+    for i in range(N-2):
+        A1[(i+1)*N] = 0
+        A1[(i+1)*N, (i+1)*N] = 1
+        A1[(i+2)*N-1] = 0
+        A1[(i+2)*N-1, (i+2)*N-1] = 1
+    
     A2 = L*dt
+    
+    A2[0:N] = 0
+    A2[-N:] = 0
+    for i in range(N):
+        A2[i, i] = 1
+        A2[-(i+1), -(i+1)] = 1
+    for i in range(N-2):
+        A2[(i+1)*N] = 0
+        A2[(i+1)*N, (i+1)*N] = 1
+        A2[(i+2)*N-1] = 0
+        A2[(i+2)*N-1, (i+2)*N-1] = 1
     
     inv_A1 = np.linalg.inv(A1)
     inv_A2 = np.linalg.inv(A2)
     
-    ux = np.zeros((ite, N, N))
-    uy = np.zeros((ite, N, N))
+    ux = np.zeros((ite+1, N, N))
+    uy = np.zeros((ite+1, N, N))
     
     ux[0] = np.reshape(ux_init, (N, N))
     uy[0] = np.reshape(uy_init, (N, N))
     
+    ps = np.zeros((ite, N, N))
+    
     for i in tqdm(range(ite)):
         
-        visc_x = inv_A1 @ ux_init
-        visc_y = inv_A1 @ uy_init
+        visc_x = inv_A1 @ (ux_init + fxx*dt)
+        visc_y = inv_A1 @ (uy_init + fyy*dt)
         
         adv_x, adv_y = advection_term(visc_x, visc_y)
         
         pres_term = del_x @ adv_x + del_x @ adv_y
         
+        pres_term[0:N] = 0
+        pres_term[-N:] = 0
+        # pres_term[N**2//2] = 1000
+        for j in range(N-2):
+            pres_term[(j+1)*N] = 0
+            pres_term[(j+2)*N-1] = 0
+        
+        
         p = inv_A2 @ pres_term
+        
+        ps[i] = np.reshape(p, (N, N))
         
         ux_init = adv_x - dt * del_x @ p
         uy_init = adv_y - dt * del_y @ p
@@ -110,4 +177,56 @@ def accretion_disc(N, x_start, x_end, t_start, t_end, dt, v, ite):
         ux[i+1] = np.reshape(ux_init, (N, N))
         uy[i+1] = np.reshape(uy_init, (N, N))
     
-    return ux, uy
+    return x, ux, uy, ps
+
+#%%
+
+trial = accretion_disc(71, -20, 20, 0.001, 0.2, 50)
+
+#%%
+from IPython.display import clear_output
+
+
+plt.close('all')
+xx, yy = np.meshgrid(trial[0], trial[0])
+
+plt.close('all')
+fig = plt.figure(0)
+ax = fig.add_subplot(1,1,1)
+plt.plot(0,0, 'o')
+t = range(len(trial[1]))
+
+for i in t:
+    ax.cla()
+    ax.quiver(xx,yy,trial[1][i],trial[2][i])
+    clear_output(wait = True)
+    plt.pause(0.05)
+    
+#%%
+plt.close('all')
+fig1 = plt.figure(1)
+ax1 = fig1.add_subplot(1,1,1)
+t1 = range(len(trial[3]))
+mi = np.min(trial[3])
+ma = np.max(trial[3])
+
+for i in t1:
+    ax1.cla()
+    ax1.imshow(trial[3][i], vmin = mi, vmax = ma, cmap='jet')
+    clear_output(wait = True)
+    plt.pause(0.1)
+
+#%%
+plt.close('all')
+fig2 = plt.figure(2)
+ax2 = fig2.add_subplot(1,1,1)
+
+for i in t:
+    ax2.cla()
+    ax2.streamplot(xx,yy,trial[1][i],trial[2][i])
+    clear_output(wait = True)
+    plt.pause(0.1)
+    
+#%%
+plt.close('all')
+plt.quiver(xx,yy,trial[1][2],trial[2][2])
